dda90713eb40e6016d9188a6cc9c9581f6369537
Tanner Wells <twells830@gmail.com>
2016-01-15 20:52:34 -0500

    Initial Commit

---
+++ POND Magazine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POND Magazine
+//
+//  Created by Tanner Wells on 1/15/16.
+//  Copyright © 2016 PONDMAG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
