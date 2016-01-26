7cea3e3de14de84e5adb50b692f0fb9ac244f888
Tanner Wells <twells830@gmail.com>
2016-01-25 21:11:47 -0500

    Added Submissions and About screens

---
+++ POND Magazine/AboutViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FeaturedController.swift
+//  POND Magazine
+//
+//  Created by Tanner Wells on 1/15/16.
+//  Copyright © 2016 PONDMAG. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if revealViewController() != nil {
+            revealViewController().rearViewRevealWidth = 200
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+            
+            //revealViewController().rightViewRevealWidth = 200
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+            
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
