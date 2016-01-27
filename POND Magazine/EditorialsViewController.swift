73c98519e52adfc96b49913cd522ccfbd9982f6e
Tanner Wells <twells830@gmail.com>
2016-01-26 23:00:25 -0500

    Figured out submenus
    Created first functional submenu for Fashion!(*@&!@(*&@
    Seems to be going well but look at updating the images and the font and
    color of the labels

--- POND Magazine/EditorialsViewController.swift
+++ POND Magazine/EditorialsViewController.swift
@@ -1,16 +1,18 @@
+
 //
-//  FeaturedController.swift
+//  EditorialViewController.swift
 //  POND Magazine
 //
-//  Created by Tanner Wells on 1/15/16.
+//  Created by Tanner Wells on 1/26/16.
 //  Copyright © 2016 PONDMAG. All rights reserved.
 //
 
 import UIKit
 
-class FashionViewController: UITableViewController, UIGestureRecognizerDelegate{
+class EditorialsViewController: UIViewController {
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +25,9 @@ class FashionViewController: UITableViewController, UIGestureRecognizerDelegate{
             //revealViewController().rightViewRevealWidth = 200
             
             view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-            
-            let tapGesture = UITapGestureRecognizer(target: self, action: "imageTapped:")
-            
-            // add it to the image view;
-            //EditorialsThumb.addGestureRecognizer(tapGesture)
-            // make sure imageView can be interacted with by user
-            //EditorialsThumb.userInteractionEnabled = true
-        }
-        
-        func imageTapped(gesture: UIGestureRecognizer) {
-            performSegueWithIdentifier("editorialPush", sender: self)
+                        
         }
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
